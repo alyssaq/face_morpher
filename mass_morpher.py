59e5c0e919dec9ad604e6a3e2d91af2ee984a580
Alyssa <alyssaquek@gmail.com>
2015-03-07 17:17:37 +0800

    sharpen image at the end

--- mass_morpher.py
+++ mass_morpher.py
@@ -27,6 +27,10 @@ def list_imgpaths(imgfolder):
         fname.lower().endswith('.jpeg')):
       yield os.path.join(imgfolder, fname)
 
+def sharpen(img):
+  blured = cv2.GaussianBlur(img, (0, 0), 2.5)
+  return cv2.addWeighted(img, 1.4, blured, -0.4, 0)
+
 def main():
   args = docopt(__doc__, version='Morpher 1.0')
   imgpaths = list(list_imgpaths(args['--images'])) #list(list_imgpaths('males')) #+ list(list_imgpaths('females'))
@@ -34,14 +38,13 @@ def main():
 
   face_points_func = partial(morpher.face_points, args['--data'])
   percent = 1 / (len(imgpaths) + 1.0)
-  print percent
   basepath = 'base/female_average.jpg'
   dst_img = scipy.ndimage.imread(basepath)
   base_points = face_points_func(basepath)
   base_img = np.copy(dst_img)
 
   passed = failed = 0
-  #dst_img = np.asarray(dst_img * percent, np.uint8)
+  dst_img = np.asarray(dst_img * percent, np.uint8)
   for i, path in enumerate(imgpaths):
     print i, path
 
@@ -53,10 +56,13 @@ def main():
       continue
 
     result_img = morpher.warp_image(src_img, src_points, base_img, base_points)
-    dst_img = cv2.addWeighted(dst_img, 0.6, result_img, 0.4, 0)
-    #dst_img += np.asarray(result_img * percent, np.uint8)
+    #dst_img = cv2.addWeighted(dst_img, 0.6, result_img, 0.4, 0)
+    dst_img += np.asarray(result_img * percent, np.uint8)
     passed += 1
 
+  dst_img = sharpen(dst_img)
+
+  #cv::addWeighted(frame, 1.5, image, -0.5, 0, image);)
   print 'Processed {0} face. {1} failed.'.format(passed, failed)
   plt.axis('off')
   plt.imshow(dst_img)
