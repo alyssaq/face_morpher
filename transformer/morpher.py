dbc3ee1533ba6a6c4b76ff36bffa7e3df4e411ac
Alyssa <alyssaquek@gmail.com>
2015-03-11 14:49:08 +0800

    More options for creating a morph

--- transformer/morpher.py
+++ transformer/morpher.py
@@ -4,51 +4,55 @@
   Morph from source to destination face
 
   Usage:
-    morpher.py --src=<src_path> --dest=<dest_path> --num=<num_frames>
-              [--blend] [--out=<frames_folder>] [--data=<classifiers_folder>]
+    morpher.py --src=<src_path> --dest=<dest_path> [--num=<num_frames>]
+              [--blend] [--out_frames=<folder>] [--out_video=<filename>]
+              [--data=<classifiers_folder>]
 
   Options:
-    -h, --help             Show this screen.
-    --src=<src_imgpath>    Filepath to source image (.jpg, .jpeg, .png)
-    --dest=<dest_path>     Filepath to destination image (.jpg, .jpeg, .png)
-    --num=<num_frames>     Number of frames to produce morph transition
-    --out=<frames_folder>  Folder path to save all image frames
-    --blend                Flag to blend images after averaging (default: False)
-    --data=<folder>        Folder to .xmls for classifiers (default: data)
-    --version              Show version.
+    -h, --help              Show this screen.
+    --src=<src_imgpath>     Filepath to source image (.jpg, .jpeg, .png)
+    --dest=<dest_path>      Filepath to destination image (.jpg, .jpeg, .png)
+    --num=<num_frames>      Number of morph frames (default: 20)
+    --out_frames=<folder>   Folder path to save all image frames
+    --out_video=<filename>  Filename to save a video (e.g. output.avi)
+    --blend                 Flag to blend images (default: False)
+    --data=<folder>         Folder to .xmls for classifiers (default: data)
+    --version               Show version.
 """
 
 from docopt import docopt
 import scipy.ndimage
 import numpy as np
 from functools import partial
+import locator
+import aligner
+import warper
+import blender
 import plotter
+import videoer
 
 def plot_morph(data_folder, src_path, dest_path,
-               num_frames=20, blend=False, out_folder=None):
-  import scipy.misc
-  import locator
-  import aligner
-  import warper
-  import blender
+               num_frames=20, blend=False, out_frames=None, out_video=None):
 
+  size = (600, 500)
+  video = videoer.Video(out_video, num_frames, (500,600))
   num_frames += (1 if blend else 0)
   plt = plotter.Plotter(True, num_images=num_frames)
   num_frames -= 2  # No need to plot/save src and dest image
 
   # Load source image
   face_points_func = partial(locator.face_points, data_folder)
-  src_img = scipy.ndimage.imread(src_path)[:, :, :3]
+  src_img = scipy.ndimage.imread(src_path)[..., :3]
   src_points = face_points_func(src_path)
-  dest_img = scipy.ndimage.imread(dest_path)[:, :, :3]
+  dest_img = scipy.ndimage.imread(dest_path)[..., :3]
   dest_points = face_points_func(dest_path)
 
-  size = (600, 500)
   src_img, src_points = aligner.resize_align(src_img, src_points, size)
   dest_img, dest_points = aligner.resize_align(dest_img, dest_points, size)
   mask = blender.mask_from_points(size, dest_points)
 
   plt.plot_one(src_img)
+  video.write(src_img)
   for percent in np.linspace(1, 0, num=num_frames):
     points = locator.weighted_average_points(src_points, dest_points, percent)
     src_face = warper.warp_image(
@@ -57,9 +61,11 @@ def plot_morph(data_folder, src_path, dest_path,
       dest_img, dest_points, points, dest_img.shape[:2])
     average_face = blender.weighted_average(src_face, end_face, percent)
     plt.plot_one(average_face, 'save')
+    video.write(average_face)
   plt.plot_one(dest_img)
+  video.write(dest_img)
 
-  if do_blend:
+  if blend:
     print 'blending'
     blended_img = blender.poisson_blend(src_face, dest_img, mask)
     plt.plot_one(blended_img)
