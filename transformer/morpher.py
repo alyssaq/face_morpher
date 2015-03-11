a3b24bff9dbb7c1c72d5448b4ae8995715705cbc
Alyssa <alyssaquek@gmail.com>
2015-03-11 13:52:56 +0800

    save frames with Plotter

--- transformer/morpher.py
+++ transformer/morpher.py
@@ -21,19 +21,16 @@ import numpy as np
 from functools import partial
 import plotter
 
-def plot_morph(data_folder, src_path, dest_path, num_frames=20, do_blend=False):
+def plot_morph(data_folder, src_path, dest_path, num_frames=20, blend=False):
   import scipy.misc
   import locator
   import aligner
   import warper
   import blender
 
-  num_frames += 2
-  rows = np.ceil(np.sqrt(num_frames / 2.0))
-  cols = np.ceil(num_frames / rows)
-  plt = plotter.Plotter(True)
-  plot_one = partial(plt.plot_one, rows, cols)
-  half_frames = num_frames / 2
+  num_frames += (1 if blend else 0)
+  plt = plotter.Plotter(True, num_images=num_frames)
+  num_frames -= 2  # No need to plot/save src and dest image
 
   # Load source image
   face_points_func = partial(locator.face_points, data_folder)
@@ -45,33 +42,29 @@ def plot_morph(data_folder, src_path, dest_path, num_frames=20, do_blend=False):
   size = (600, 500)
   src_img, src_points = aligner.resize_align(src_img, src_points, size)
   dest_img, dest_points = aligner.resize_align(dest_img, dest_points, size)
-
   mask = blender.mask_from_points(size, dest_points)
-  end_face = blender.apply_mask(dest_img, mask)
 
-  plot_one(src_img)
-  for percent in np.linspace(1, 0, num=half_frames):
+  plt.plot_one(src_img)
+  for percent in np.linspace(1, 0, num=num_frames):
     points = locator.weighted_average_points(src_points, dest_points, percent)
     src_face = warper.warp_image(
       src_img, src_points, points, dest_img.shape[:2])
-    plot_one(src_face)
-
-  for percent in np.linspace(1, 0, num=half_frames):
-    ave = blender.weighted_average(src_face, end_face, percent)
-    plot_one(ave)
-
-  plot_one(dest_img)
+    end_face = warper.warp_image(
+      dest_img, dest_points, points, dest_img.shape[:2])
+    average_face = blender.weighted_average(src_face, end_face, percent)
+    plt.plot_one(average_face, 'save')
+  plt.plot_one(dest_img)
 
   if do_blend:
     print 'blending'
     blended_img = blender.poisson_blend(src_face, dest_img, mask)
-    plot_one(blended_img)
+    plt.plot_one(blended_img)
 
-  plt.end()
+  plt.show()
 
 if __name__ == "__main__":
   # args = docopt(__doc__, version='2 Image Morpher 1.0')
   # if args['--data'] is None:
   #   args['--data'] = 'data'
   # plot_morph(args['--data'], args['--src'], args['--dest'], args['--blend'])
-  plot_morph('../data', '../family/IMG_20140515_203547.jpg', '../john_malkovich.jpg', 20, False)
+  plot_morph('../data', '../family/IMG_20140515_203547.jpg', '../john_malkovich.jpg', 4, False)
--- transformer/plotter.py
+++ transformer/plotter.py
@@ -1,7 +1,11 @@
 from matplotlib import pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
 
 def check_do_plot(func):
   def inner(self, *args, **kwargs):
+    if 'save' in args:
+      mpimg.imsave('frame{0}.png'.format(self.counter[0]-1), args[0])
     if self.do_plot:
       return func(self, *args, **kwargs)
     else:
@@ -9,20 +13,27 @@ def check_do_plot(func):
   return inner
 
 class Plotter(object):
-  def __init__(self, plot=True):
+  def __init__(self, plot=True, rows=0, cols=0, num_images=0, folder=None):
     self.counter = [1]
     self.do_plot = plot
+    self.folder = folder
+    if (rows + cols) == 0 and num_images > 0:
+      self.rows = np.ceil(np.sqrt(num_images / 2.0))
+      self.cols = np.ceil(num_images / self.rows)
+    else:
+      self.rows = rows
+      self.cols = cols
 
   @check_do_plot
-  def plot_one(self, row, col, img):
-    p = plt.subplot(row, col, self.counter[0])
+  def plot_one(self, img, save=False, folder='.'):
+    p = plt.subplot(self.rows, self.cols, self.counter[0])
     p.axes.get_xaxis().set_visible(False)
     p.axes.get_yaxis().set_visible(False)
     plt.imshow(img)
     self.counter[0] += 1
 
   @check_do_plot
-  def end(self):
+  def show(self):
     plt.gcf().subplots_adjust(hspace=0.05, wspace=0,
                               left=0, bottom=0, right=1, top=0.98)
     plt.axis('off')
