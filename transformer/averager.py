2d22090a1bad5401dd20b99c70f1be7ef5e37cf7
Alyssa <alyssaquek@gmail.com>
2015-03-12 03:00:36 +0800

    Average faces

--- transformer/averager.py
+++ transformer/averager.py
@@ -26,6 +26,7 @@ from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
 
 import locator
+import aligner
 import warper
 
 def list_imgpaths(imgfolder):
@@ -39,44 +40,54 @@ def sharpen(img):
   blured = cv2.GaussianBlur(img, (0, 0), 2.5)
   return cv2.addWeighted(img, 1.4, blured, -0.4, 0)
 
-def main():
-  args = docopt(__doc__, version='Morpher 1.0')
-  imgpaths = list(list_imgpaths(args['--images']))
+def load_image_points(data_folder, path, size):
+  img = scipy.ndimage.imread(path)[..., :3]
+  points = locator.face_points(data_folder, path)
 
-  face_points_func = partial(locator.face_points, args['--data'])
-  basepath = imgpaths[5]
-  base_img = scipy.ndimage.imread(basepath)
-  base_points = face_points_func(basepath)
+  if len(points) == 0:
+    return None, None
+  else:
+    return aligner.resize_align(img, points, size)
 
-  passed = failed = 0
-  images = np.zeros(base_img.shape, np.float32)
-  for i, path in enumerate(imgpaths):
-    print i, path
+def average_faces(images_folder, data_folder, width=500, height=600,
+                  blend=False):
+  imgpaths = list(list_imgpaths(images_folder))
 
-    src_img = scipy.ndimage.imread(path)
-    src_points = face_points_func(path)
-    if len(src_points) == 0:
-      print 'No face'
-      failed += 1
-      continue
+  size = (height, width)
+  loader = partial(load_image_points, data_folder, size=size)
 
-    images += warper.warp_image(src_img, src_points,
-                                base_points, base_img.shape[:2])
-    passed += 1
+  images = []
+  point_set = []
+  for path in imgpaths:
+    img, points = loader(path)
+    if img is not None:
+      images.append(img)
+      point_set.append(points)
 
-  result_image = np.uint8(images / passed)
+  ave_points = locator.average_points(point_set)
+  num_images = len(images)
+  result_images = np.zeros(images[0].shape, np.float32)
+  for i in xrange(num_images):
+    print '{0} of {1}'.format(i+1, num_images)
 
-  if args['--blend']:
+    result_images += warper.warp_image(images[i], point_set[i],
+                                       ave_points, size)
+
+  result_image = np.uint8(result_images / num_images)
+
+  if blend:
     result_image = sharpen(result_image)
     import blender
     mask = blender.mask_from_points(base_img.shape[:2], base_points)
     result_image = blender.alpha_feathering(base_img, result_image, mask)
 
-  print 'Processed {0} face. {1} failed.'.format(passed, failed)
+  print 'Processed {0} faces'.format(num_images)
   plt.axis('off')
   plt.imshow(result_image)
   plt.show()
   mpimg.imsave('result.png', result_image)
 
 if __name__ == "__main__":
-  main()
+  args = docopt(__doc__, version='Face Averager 1.0')
+  average_faces(args['--images'], args['--data'],
+                int(args['--width']), int(args['--height']), args['--blend'])
