47390d3acdab94dfd4438faed69cbec39906bab9
alyssaq <alyssaquek@gmail.com>
2016-05-14 18:06:22 +0200

    Bump to facemorpher 2.0 which supports openCV 3 and Python 3

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from distutils.core import setup
 
 setup(
   name='facemorpher',
-  version='1.0.1',
+  version='2.0.0',
   author='Alyssa Quek',
   author_email='alyssaquek@gmail.com',
   description=('Warp, morph and average human faces!'),
   packages=['facemorpher'],
-  package_data={'facemorpher': ['data/*.xml', 'bin/stasm_util_osx', 'bin/stasm_util_linux']},
+  package_data={'facemorpher': [
+    'data/*.xml',
+    'bin/stasm_util_osx_cv2',
+    'bin/stasm_util_osx_cv3',
+    'bin/stasm_util_linux_cv2'
+  ]},
   data_files=[('readme', ['README.rst'])],
   keywords='face morphing, averaging, warping',
   url='https://github.com/alyssaq/face_morpher',
