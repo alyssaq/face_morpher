95fdb1aa1bf4709ea433cf8dccad511005b9fb96
alyssaq <alyssaquek@gmail.com>
2018-01-22 00:15:39 +0800

    import os and sys

--- scripts/make_docs.sh
+++ scripts/make_docs.sh
@@ -7,6 +7,8 @@ sphinx-apidoc -A "Alyssa Quek" -f -F -o docs facemorpher/
 cd docs
 
 # Append module path to end of conf file
+echo "import os" >> conf.py
+echo "import sys" >> conf.py
 echo "sys.path.insert(0, os.path.abspath('../'))" >> conf.py
 echo "sys.path.insert(0, os.path.abspath('../facemorpher'))" >> conf.py
 
