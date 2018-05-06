6ba352b6f0d7281669c1550a40e7314976dc8c07
alyssaq <alyssaquek@gmail.com>
2018-05-06 23:54:30 +0800

    Use dlib as a fallback face dectector if stasm does not work

--- facemorpher/locator.py
+++ facemorpher/locator.py
@@ -10,8 +10,12 @@ import os.path as path
 
 from facemorpher import cvver
 
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor('./facemorpher/data/shape_predictor_68_face_landmarks.dat')
+try:
+  import dlib
+  dlib_detector = dlib.get_frontal_face_detector()
+  dlib_predictor = dlib.shape_predictor('./facemorpher/data/shape_predictor_68_face_landmarks.dat')
+except:
+  pass
 
 # Stasm util binary in `bin` folder available for these platforms
 SUPPORTED_PLATFORMS = {
@@ -20,36 +24,25 @@ SUPPORTED_PLATFORMS = {
   'darwin': 'osx'
 }
 
-def shape_to_np(shape, dtype="int"):
-  # initialize the list of (x, y)-coordinates
-  coords = np.zeros((68, 2), dtype=dtype)
- 
-  # loop over the 68 facial landmarks and convert them
-  # to a 2-tuple of (x, y)-coordinates
-  for i in range(0, 68):
-	coords[i] = (shape.part(i).x, shape.part(i).y)
- 
-  # return the list of (x, y)-coordinates
-  return coords
-
-def boundary_points(points):
+def boundary_points(points, width_percent=0.1, height_percent=0.1):
   """ Produce additional boundary points
 
   :param points: *m* x 2 array of x,y points
+  :param width_percent: [-1, 1] percentage of width to taper inwards. Negative for opposite direction
+  :param height_percent: [-1, 1] percentage of height to taper downwards. Negative for opposite direction
   :returns: 2 additional points at the top corners
   """
   x, y, w, h = cv2.boundingRect(np.array([points], np.int32))
-  buffer_percent = 0.1
-  spacerw = int(w * buffer_percent)
-  spacerh = int(h * buffer_percent)
+  spacerw = int(w * width_percent)
+  spacerh = int(h * height_percent)
   return [[x+spacerw, y+spacerh],
           [x+w-spacerw, y+spacerh]]
 
-def face_points(imgpath, add_boundary_points=True):
+def face_points_stasm(imgpath, add_boundary_points=True):
   """ Locates 77 face points using stasm (http://www.milbo.users.sonic.net/stasm)
 
   :param imgpath: an image path to extract the 77 face points
-  :param add_boundary_points: bool to add 2 additional points
+  :param add_boundary_points: bool to add additional boundary points
   :returns: Array of x,y face points. Empty array if no face found
   """
   directory = path.dirname(path.realpath(__file__))
@@ -74,24 +67,54 @@ def face_points(imgpath, add_boundary_points=True):
   else:
     points = np.array([pair.split(' ') for pair in s.rstrip().split('\n')],
                       np.int32)
-    if (add_boundary_points):
+    if add_boundary_points:
       points = np.vstack([points, boundary_points(points)])
 
     return points
 
-# Find 68 face points using dlib
-def face_points_2(image):
-  face_points = []
+def face_points_dlib(imgpath, add_boundary_points=True):
+  """ Locates 68 face points using dlib (http://dlib.net)
+
+  :param imgpath: an image path to extract the 68 face points
+  :param add_boundary_points: bool to add additional boundary points
+  :returns: Array of x,y face points. Empty array if no face found
+  """
   try:
-    rects = detector(image, 1)
-    if rects:
-        # We only take the first found face
-        rect = rects[0]
-        points = predictor(image, rect)
-        points = shape_to_np(points)
-  except:
+    points = []
+    image = cv2.imread(imgpath)
+    rgbimg = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    rects = dlib_detector(rgbimg, 1)
+
+    if rects and len(rects) > 0:
+      # We only take the first found face
+      shapes = dlib_predictor(rgbimg, rects[0])
+      points = np.array([(shapes.part(i).x, shapes.part(i).y) for i in range(68)], np.int32)
+
+      if add_boundary_points:
+        # Add more points inwards and upwards as dlib only detects up to eyebrows
+        points = np.vstack([
+          points,
+          boundary_points(points, 0.1, -0.05),
+          boundary_points(points, 0.13, -0.09),
+          boundary_points(points, 0.15, -0.14),
+          boundary_points(points, 0.33, -0.2)])
+
     return points
-  return face_points
+  except Exception as e:
+    print(e)
+    return []
+
+def face_points(imgpath, add_boundary_points=True):
+  """ High-level function to detect face points using stasm then dlib
+
+  :param imgpath: an image path to extract face points
+  :param add_boundary_points: bool to add additional boundary points
+  :returns: Array of x,y face points. Empty array if no face found
+  """
+  points = []#face_points_stasm(imgpath, add_boundary_points)
+  if len(points) == 0:
+    points = face_points_dlib(imgpath, add_boundary_points)
+  return points
 
 def average_points(point_set):
   """ Averages a set of face points from images
