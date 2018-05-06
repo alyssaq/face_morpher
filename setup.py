77c953aad2952c6c8da54ab0bbbf58f75180fc89
alyssaq <alyssaquek@gmail.com>
2018-05-07 00:15:01 +0800

    Added dlib and face landmarks

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ class OverrideInstall(install):
 
 setup(
   name='facemorpher',
-  version='3.4.2',
+  version='3.4.0b0',
   author='Alyssa Quek',
   author_email='alyssaquek@gmail.com',
   description=('Warp, morph and average human faces!'),
@@ -25,6 +25,7 @@ setup(
   packages=find_packages(),
   package_data={'facemorpher': [
     'data/*.xml',
+    'data/shape_predictor_68_face_landmarks.dat',
     'bin/stasm_util_osx_cv3.2',
     'bin/stasm_util_osx_cv3.4',
     'bin/stasm_util_linux_cv3.2',
@@ -38,6 +39,7 @@ setup(
     'Pillow',
     'future',
     'opencv-python',
+    'dlib',
   ],
   cmdclass={'install': OverrideInstall},
   entry_points={'console_scripts': [
